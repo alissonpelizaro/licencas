5dc02cfcfcb240df5d1cbf03503977559fa68cfa
Alisson Pelizaro <dev@enterness.com>
2019-02-26 11:19:24 -0300

    Initial Commit

---
+++ application/controllers/Login.php
@@ -0,0 +1,59 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Login extends CI_Controller {
+
+
+		public function index(){
+
+			$this->session->sess_destroy();
+			if($this->session->userdata('id')){
+				redirect('../inicio');
+			} else {
+				$this->load->view('login');
+			}
+
+		}
+
+		public function execLogin(){
+			$this->load->model('login_model');
+
+			$usr = $this->input->post('login');
+			$passPure = $this->input->post('password');
+			$pass = md5($passPure);
+
+			$usr = $this->login_model->checkLogin($usr, $pass);
+
+
+			if($usr){
+
+				$this->session->set_userdata('id', $usr->idUser);
+				$this->session->set_userdata('nome', $usr->nome);
+				$this->session->set_userdata('senha', $usr->senha);
+				$this->session->set_userdata('hora', date('Y-m-d H:i:s'));
+
+				redirect('../inicio');
+
+			} else {
+
+				$this->load->view('login', array('invalidUser' => true));
+
+			}
+		}
+
+		public function logout(){
+				$this->load->library('log_lib');
+				$this->load->model('login_model');
+
+				$this->log_lib->setAcao('Saiu do sistema');
+				$this->log_lib->setFerramenta('MyOmni');
+				$this->log_lib->gravaLog();
+
+				$this->login_model->deslogar($this->session->userdata('id'));
+
+				$this->session->sess_destroy();
+
+				redirect('../');
+
+		}
+}
---
+++ assets/inc/scripts.php
@@ -0,0 +1,6 @@
+<!-- Argon Scripts -->
+<!-- Core -->
+<script src="<?php echo base_url(); ?>assets/vendor/jquery/dist/jquery.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+<!-- Argon JS -->
+<script src="<?php echo base_url(); ?>assets/js/argon.js?v=1.0.0"></script>
